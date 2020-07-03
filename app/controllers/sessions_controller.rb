03cec1994c9c14e76f4666c9bc586dfa0ae49242
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-03 19:53:11 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        if params[:name].nil? || params[:name].empty?
+            redirect_to login_path
+        elsif session[:name]
+            redirect_to root_path
+        else
+            session[:name] = params[:name]
+            redirect_to root_path
+        end
+    end
+
+    def destroy
+        session.delete :name
+    end
+
+end
